7a8e669c56bb7a7e7cefcfcfdac81c89411273e1
kiminzajnr <kiminzaeric@gmail.com>
2022-07-30 07:31:20 +0300

    revamp repo and remove unnecessary folders

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="kiminza/demolocal"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment demolocal-first --image=kiminza/demolocal
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/demolocal-first 8000:80
